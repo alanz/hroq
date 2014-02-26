b16c305cffcaf0d989de350859c070357c52d472
Alan Zimmerman <alanz@alanz-laptop>
2014-02-26 09:28:57 +0200

    Use Generic to derive Binary instances

--- src/Data/Hroq.hs
+++ src/Data/Hroq.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE DeriveDataTypeable  #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveDataTypeable   #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TemplateHaskell      #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module Data.Hroq
@@ -43,6 +45,7 @@ import Data.Maybe
 import Data.RefSerialize
 import Data.Time.Clock
 import Data.Typeable
+import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as C8
 import qualified Data.Map as Map
 
@@ -57,31 +60,18 @@ maxBucketSizeConst = 50
 -- ---------------------------------------------------------------------
 
 data ConsumerName = CN !String
-                    deriving (Eq,Show,Read,Typeable,Ord)
+                    deriving (Eq,Show,Read,Typeable,Ord,Generic)
 instance Binary ConsumerName where
-  put (CN n) = put n
-  get = liftM CN get
-
 
 data QName = QN !String
-             deriving (Typeable,Show,Read,Eq,Ord)
-
+             deriving (Typeable,Show,Read,Eq,Ord,Generic)
 instance Binary QName where
-  put (QN s) = put s
-  get = do
-    s <- get
-    return (QN s)
 
 -- ---------------------------------------------------------------------
 
 data QKey = QK !String
-            deriving (Typeable,Show,Read,Eq,Ord)
-
+            deriving (Typeable,Show,Read,Eq,Ord,Generic)
 instance Binary QKey where
-  put (QK i) = put i
-  get = do
-    i <- get
-    return $ QK i
 
 consumerNameKey :: ConsumerName -> QKey
 consumerNameKey (CN s) = QK s
@@ -106,37 +96,13 @@ data QValue = QVP Payload
             | QVC ConsumerMessage
             | QVD DlqMessage
             | QVM Meta
-            deriving (Typeable,Read,Show,Eq)
+            deriving (Typeable,Read,Show,Eq,Generic)
+instance Binary QValue where
 
 data QEntry = QE !QKey    -- ^Id
                  !QValue  -- ^payload
-              deriving (Typeable,Read,Show,Eq)
-
-instance Binary QValue where
-  put (QVP m) = put 'P' >> put m
-  put (QVC m) = put 'C' >> put m
-  put (QVD m) = put 'D' >> put m
-  put (QVM m) = put 'M' >> put m
-
-  get = do 
-    sel <- get
-    case sel of
-      'P' -> liftM QVP get
-      'C' -> liftM QVC get
-      'D' -> liftM QVD get
-      'M' -> liftM QVM get
-{-
-instance Binary QValue where
-  put (QV v) = put v
-  get = liftM QV get
--}
-
+              deriving (Typeable,Read,Show,Eq,Generic)
 instance Binary QEntry where
-  put (QE k v) = put k >> put v
-  get = do
-    k <- get
-    v <- get
-    return $ QE k v 
 
 instance Serialize QEntry where
   showp = showpBinary
@@ -153,27 +119,14 @@ instance Binary NominalDiffTime where
 type MetaKey = QName
 
 data Meta = MAllBuckets !MetaKey ![TableName] !TimeStamp
-            deriving (Show,Read,Typeable,Eq)
-
+            deriving (Show,Read,Typeable,Eq,Generic)
 instance Binary Meta where
-  put (MAllBuckets q tns ts) = put q >> put tns >> put ts
-  get = do
-    q <- get
-    tns <- get
-    ts <- get
-    return $ MAllBuckets q tns ts
-
 
 -- ---------------------------------------------------------------------
 
 data TableName = TN !String
-                 deriving (Show,Read,Typeable,Eq,Ord)
-
+                 deriving (Show,Read,Typeable,Eq,Ord,Generic)
 instance Binary TableName where
-  put (TN s) = put s
-  get = do
-    s <- get
-    return (TN s)
 
 -- ---------------------------------------------------------------------
 
@@ -185,54 +138,35 @@ data OverflowBucket = OB !QName
 
 
 data TimeStamp = TS !String
-                 deriving (Show,Read,Eq)
-
+                 deriving (Show,Read,Eq,Generic)
 instance Binary TimeStamp where
-  put (TS s) = put s
-  get = do
-    s <- get
-    return (TS s)
 
 -- ---------------------------------------------------------------------
 
 -- data Payload = Payload !(Map.Map String String)
 data Payload = Payload !String
-              deriving (Typeable,Read,Show,Eq)
+              deriving (Typeable,Read,Show,Eq,Generic)
 instance Binary Payload where
-  put (Payload p) = put p
-  get = liftM Payload get
 
 -- -record(eroq_consumer_message,  {cid, key, msg, src_queue, timestamp = now()}).
 
 data ConsumerMessage = CM ConsumerName QKey QEntry QName TimeStamp
-                       deriving (Typeable,Read,Show,Eq)
+                       deriving (Typeable,Read,Show,Eq,Generic)
 instance Binary ConsumerMessage where
-  put (CM cn key val srcQueue ts) = put cn >> put key >> put val >> put srcQueue >> put ts
-  get = liftM5 CM get get get get get
 
 -- ---------------------------------------------------------------------
 
 data DlqMessage = DM !String !QValue -- ^Reason, original message
-                  deriving (Typeable,Read,Show,Eq)
-
+                  deriving (Typeable,Read,Show,Eq,Generic)
 instance Binary DlqMessage where
-  put (DM reason msg) = put reason >> put msg
-  get = liftM2 DM get get
-
 
 -- ---------------------------------------------------------------------
 
 -- |Signal sent from enqueue to consumer to notify of freshly enqueued
 -- message
 data QueueMessage = QueueMessage
-                     deriving (Typeable,Show)
-
+                     deriving (Typeable,Show,Generic)
 instance Binary QueueMessage where
-  put QueueMessage = put 'Q'
-  get = do
-    sel <- get
-    case sel of
-      'Q' -> return QueueMessage
 
 
 
